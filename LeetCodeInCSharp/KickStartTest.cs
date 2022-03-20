3f69eb52d228356ab48455868d98d01efe6ba83a
shiminliu <shiminliu@microsoft.com>
2022-03-20 20:35:29 +0800

    test for kickstart

---
+++ LeetCodeInCSharp/KickStartTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int cou = Convert.ToInt32(Console.ReadLine());
+			for (int i = 0; i < cou; i++)
+			{
+				string result = $"Case #{i + 1}: ";
+
+				var line1 = Console.ReadLine().Split(' ');
+				int budget = Convert.ToInt32(line1[1]);
+
+				var housePrices = Console.ReadLine().Split(' ').Select(s => Convert.ToInt32(s)).OrderBy(p => p).ToList();
+				int buyCou = 0, total = 0;
+				for (int j = 0; j < housePrices.Count(); j++)
+				{
+					total += housePrices[j];
+					if (total <= budget)
+						buyCou++;
+					else
+						break;
+				}
+
+				Console.WriteLine(result + buyCou);
+			}
+		}
+	}
+}
---
+++ LeetCodeInCSharp/KickStartTest_1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+	class Program
+	{
+		// https://codingcompetitions.withgoogle.com/kickstart/round/00000000008cb33e/00000000009e7021 test for c# ocmmit
+		/*
+		 input
+		    2
+		    Ilovecoding
+			IIllovecoding
+			KickstartIsFun
+			kkickstartiisfun
+		output:
+			Case #1: 2
+			Case #2: IMPOSSIBLE
+		 */
+		static void Main(string[] args)
+		{
+			int T = Convert.ToInt32(Console.ReadLine());
+			for (int j = 0; j < T; j++)
+			{
+				string result = $"Case #{j + 1}: ";
+
+				string s = Console.ReadLine();
+				string t = Console.ReadLine();
+				int r = 0;
+				int res = t.Length - s.Length;
+				for (int i = 0; i < s.Length; i++)
+                {
+					while (r < t.Length && t[r] != s[i]) r++;
+					if (r == t.Length)
+					{
+						res = -1;
+						break;
+					}
+					r++;
+				}
+					
+				Console.WriteLine(result + (res == -1 ? "IMPOSSIBLE" : res.ToString()));
+			}
+		}
+	}
+}
