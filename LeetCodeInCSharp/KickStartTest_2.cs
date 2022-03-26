c5a2b42db29a702ba359e08ab875ba2e19f7fea1
shiminliu <shiminliu@microsoft.com>
2022-03-26 17:48:09 +0800

    number and use kickstart 999

---
+++ LeetCodeInCSharp/KickStartTest_2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+	public class Program_chanllenge_nine
+	{
+		// https://codingcompetitions.withgoogle.com/kickstart/round/00000000008cb33e/00000000009e7997 chanllenge nine
+		/*
+		 能被9整除的数 各个数位加起来也能被9整除
+		 */
+		static void Main(string[] args)
+		{
+			int T = Convert.ToInt32(Console.ReadLine());
+			for (int j = 0; j < T; j++)
+			{
+				string result = $"Case #{j + 1}: ";
+
+				string str = Console.ReadLine();
+				int rem = 0;
+				foreach(char c in str)
+                {
+					rem = (rem + (c - '0')) % 9;
+                }
+				int d = (9 - rem) % 9;
+				int pos = d == 0 ? 1 : 0;
+				while ( pos < str.Length && str[pos] - '0' <= d)
+                {
+					pos++;
+                }
+				str = str.Insert(pos, d.ToString());
+					
+				Console.WriteLine(result + str);
+			}
+		}
+	}
+}
--- LeetCodeInCSharpUnitTest/UnitTest0305_code.cs
+++ LeetCodeInCSharpUnitTest/UnitTest0305_code.cs
@@ -10,11 +10,12 @@ namespace LeetCodeInCSharpUnitTest
         [TestMethod]
         public void TestSolution()
         {
-            var solution = new Solution();
-            solution.MinimalKSum(new int[] { 5, 6 }, 6);
+            // var solution = new Solution();
+            // solution.MinimalKSum(new int[] { 5, 6 }, 6);
             // solution.MinimalKSum(new int[]{96, 44, 99, 25, 61, 84, 88, 18, 19, 33, 60, 86, 52, 19, 32, 47, 35, 50, 94, 17, 29, 98, 22, 21, 72, 100, 40, 84}, 35);
             // var ans = solution.SortJumbled(new int[] {8, 9, 4, 0, 2, 1, 3, 5, 7, 6}, new int[] { 991, 338, 38 });
-
+            var kick = new Program_chanllenge_nine();
+            
         }
     }
 }
