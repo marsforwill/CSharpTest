a13a745bbf8fd0a6cc6613de60aef3916620262f
Mars <529714831@qq.com>
2024-02-19 07:57:14 +0000

    eightqueen

---
+++ LeetCodeInCSharp/0219_eightQueen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+	public class EightQueen
+	{
+		public static int N = 8;
+		public static []int position = new int[N];
+		static void Main(string[] args)
+		{
+			solve(0);
+		}
+
+		public static void solve(int k){
+			if (k == N){
+				Console.WriteLine(string.Join(", ", position));
+				return;
+			}
+
+			for (int i = 0; i < N; i++){
+				if (!conflict(k, i)){
+					position[k] = i;
+					solve(k+1)
+				}
+			}
+		}
+
+		// Check if placing a queen at column 'p' in row 'k' is safe
+		// i: other
+		// k: p
+		public static bool conflict(int k, int p){
+			for(int i = 0; i < k; i++){
+				int other = position[i];
+				if (other == p || (k-i == p-other) || (k-i == other-p) ){
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
